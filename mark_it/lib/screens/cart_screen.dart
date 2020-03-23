5e4190ddef5c4d05ba4880106c1dc432525639c8
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-03-23 18:48:38 +0530

    v0.3

--- mark_it/lib/screens/cart_screen.dart
+++ mark_it/lib/screens/cart_screen.dart
@@ -13,38 +13,54 @@ class CartScreen extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Card(
-              margin: EdgeInsets.all(15),
-              child: Padding(
-                padding: EdgeInsets.all(8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Total',
-                      style: TextStyle(fontSize: 20),
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                          color:
+                              Theme.of(context).primaryTextTheme.title.color),
                     ),
-                    Spacer(),
-                    Chip(
-                      label: Text(
-                        '\$${cart.totalAmount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                            color:
-                                Theme.of(context).primaryTextTheme.title.color),
-                      ),
-                      backgroundColor: Theme.of(context).primaryColor,
-                    ),
-                    FlatButton(
-                      child: Text('Order Now'),
-                      textColor: Theme.of(context).primaryColor,
-                      onPressed: () {
-                        Provider.of<Orders>(context, listen: false).addOrder(
-                            cart.items.values.toList(), cart.totalAmount);
-                        cart.clearCart();
-                      },
-                    )
-                  ],
-                ),
-              ))
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  FlatButton(
+                    child: Text('Order Now'),
+                    textColor: Theme.of(context).primaryColor,
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                          cart.items.values.toList(), cart.totalAmount);
+                      cart.clearCart();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (ctx, index) => CartItem(
+                cart.items.values.toList()[index].id,
+                cart.items.keys.toList()[index],
+                cart.items.values.toList()[index].title,
+                cart.items.values.toList()[index].quantity,
+                cart.items.values.toList()[index].price,
+              ),
+              itemCount: cart.itemCount,
+            ),
+          )
         ],
       ),
     );
--- mark_it/lib/widgets/product_item.dart
+++ mark_it/lib/widgets/product_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/product.dart';
 import '../screens/product_detail_screen.dart';
+import '../providers/cart.dart';
 
 class ProductItem extends StatelessWidget {
   // final String id;
@@ -12,6 +13,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context,listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -41,7 +43,24 @@ class ProductItem extends StatelessWidget {
               Icons.shopping_cart,
               color: Theme.of(context).accentColor,
             ),
-            onPressed: () {},
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Added ${product.title} to Cart',
+                  ),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'Undo',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
+            },
           ),
           title: Text(
             product.title,
