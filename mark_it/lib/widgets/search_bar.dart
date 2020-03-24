d8226c23fd1943501dec9c92400cd9a98f3e7b5a
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-03-24 16:54:02 +0530

    Update search_bar.dart
    added cancel functionality

--- mark_it/lib/widgets/search_bar.dart
+++ mark_it/lib/widgets/search_bar.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:mark_it/providers/products.dart';
 import 'package:provider/provider.dart';
 
-class SearchBar extends StatelessWidget {
+class SearchBar extends StatefulWidget {
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  final _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final products = Provider.of<Products>(context);
@@ -11,13 +18,26 @@ class SearchBar extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 2),
-            width: MediaQuery.of(context).size.width,
+            height: 60,
+            width: MediaQuery.of(context).size.width - 20,
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+            alignment: Alignment.center,
             child: TextFormField(
+              controller: _searchController,
               autocorrect: true,
-              decoration: InputDecoration(labelText: 'Search Product'),
+              decoration: InputDecoration(
+                labelText: 'Search Product',
+                labelStyle: TextStyle(fontSize: 10),
+                suffix: IconButton(
+                  icon: Icon(Icons.cancel),
+                  onPressed: () {
+                    _searchController.clear();
+                    products.checkSubString('');
+                  },
+                ),
+              ),
               textInputAction: TextInputAction.done,
-              onFieldSubmitted: (value) {
+              onChanged: (value) {
                 products.checkSubString(value);
               },
             ),
