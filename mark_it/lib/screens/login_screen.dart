dd6811b4e4f439f46f90e2363199e70ff32f06c5
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-04-16 22:32:19 +0530

    Update login_screen.dart

--- mark_it/lib/screens/login_screen.dart
+++ mark_it/lib/screens/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:mark_it/screens/home_screen.dart';
 import '../screens/home_screen.dart';
 import 'ProfileScreen.dart';
 
@@ -130,7 +129,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           child: Text('Skip'),
                           onPressed: () {
                             Navigator.of(context)
-                                .pushReplacementNamed(Home.routeName);
+                                .pushReplacementNamed(HomeScreen.routeName);
                           },
                         ),
                       ),
