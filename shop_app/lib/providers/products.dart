4a080e98ac3064aae35a1ef5f197f75666b70266
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-05-05 19:35:18 +0530

    attaching products to users

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -62,9 +62,11 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  Future<void> fetchAndSetProducts() async {
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    final filterString =
+        filterByUser ? 'orderBy="creatorId"&equalTo="$userId"' : '';
     var url =
-        'https://shop-app-e57d2.firebaseio.com/products.json?auth=$authToken';
+        'https://shop-app-e57d2.firebaseio.com/products.json?auth=$authToken&$filterString';
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -116,6 +118,7 @@ class Products with ChangeNotifier {
           'description': product.description,
           'imageUrl': product.imageUrl,
           'price': product.price,
+          "creatorId": userId,
         }),
       );
       final newProduct = Product(
